8c126956b8e80cdf0247800ab474665bd32d1058
zjimenez110492 <zjimenez@unicauca.edu.co>
2020-07-12 19:43:19 -0500

    formGroups, buttons dentro de formularios, getFormControl, submit

--- src/app/components/product-form/product-form.component.ts
+++ src/app/components/product-form/product-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import{FormControl, Validators} from '@angular/forms'
+import{FormControl, Validators, FormGroup, FormBuilder} from '@angular/forms'
 import { debounceTime, debounce } from 'rxjs/operators';
 
 
@@ -9,24 +9,83 @@ import { debounceTime, debounce } from 'rxjs/operators';
   styleUrls: ['./product-form.component.css']
 })
 export class ProductFormComponent implements OnInit {
-emailCtrl= new FormControl('',[Validators.required, Validators.maxLength(20), Validators.minLength(11)]);
-  constructor() { 
-    this.emailCtrl.valueChanges.pipe(
-    debounceTime(350)
-    ).subscribe(
-      value=>{
-        console.log(value);
-        
-      }
-    )
-    
+  
+  form: FormGroup;
+  tamText=0;
+  constructor(private FormBuilder:FormBuilder) { 
+    this.buildForm();
   }
   ngOnInit() {
   }
-  getEmail(event:Event)
+  private buildForm() {
+    this.form= this.FormBuilder.group({
+      name:['',[Validators.required]],
+      date:['',[Validators.required]],
+      email:['',[Validators.email]],
+      text:['',[Validators.maxLength(200),Validators.required],],      
+      category:['',[Validators.required]],      
+      gender:['',[Validators.required]]  
+    });
+
+    
+    /* Builder con new
+    this.form= new FormGroup({
+      name:new FormControl('',[Validators.required]),
+      date:new FormControl('',[Validators.required]),
+      email:new FormControl('',[Validators.email]),
+      text:new FormControl('',[Validators.maxLength(200)]),      
+      category:new FormControl('',[Validators.required]),      
+      gender:new FormControl('',[Validators.required])  
+    }); */
+    this.form.valueChanges.pipe(
+      debounceTime(350)
+      ).subscribe(
+        value=>{
+          console.log(value);
+          console.log("El formulario es valido?:  "+this.form.valid);
+          let text=this.form.controls['text'].value;
+          this.tamText=text.length;
+        }
+      )
+  }
+ /*  getEmail(event:Event)
   {
     event.preventDefault();
     console.log(this.emailCtrl.value);
   }
-
+ */
+  save(event:Event)
+  {
+    event.preventDefault(); //se Cancela su comportamiento por defecto, evitando que se recargue la pagina
+    if(this.form.valid)
+    {
+      const value= this.form.value;
+      console.log(value);  
+    }
+    else this.form.markAllAsTouched();
+    }
+    getNameField()
+    {
+      this.form.get('name');
+    }
+    getDateField()
+    {
+      this.form.get('date');
+    }
+    getEmailField()
+    {
+      this.form.get('email');
+    }
+    getTextField()
+    {
+      this.form.get('text');
+    }
+    getCategoryField()
+    {
+      this.form.get('category');
+    }
+    getGenderField()
+    {
+      this.form.get('gender');
+    }
 }
